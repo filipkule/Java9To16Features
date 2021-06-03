834fb38a7df89f2b15998042248495a6edce162a
Filip Kulevski <filipkule@gmail.com>
2021-06-04 00:27:53 +0200

    Created private static method annotated with @SafeVarargs and varargs as input

---
+++ src/main/java/solutions/Issue3SafeVarargsAnnotation.java
@@ -0,0 +1,14 @@
+package solutions;
+
+public class Issue3SafeVarargsAnnotation {
+    public static void main(String[] args) {
+        printAllArgs("Hello ", "Stranger!");
+    }
+
+    @SafeVarargs
+    private static void printAllArgs(String... inputArgs){
+        for (String argument: inputArgs) {
+            System.out.print(argument);
+        }
+    }
+}
