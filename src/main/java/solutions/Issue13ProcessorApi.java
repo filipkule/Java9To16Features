949e5753e5262ca7f3457f0ff6f4f4d54527124e
Filip Kulevski <filipkule@gmail.com>
2021-06-26 13:24:29 +0200

    Process api improvments example

---
+++ src/main/java/solutions/Issue13ProcessorApi.java
@@ -0,0 +1,36 @@
+package solutions;
+
+import java.io.IOException;
+
+public class Issue13ProcessorApi {
+    public static void main(String[] args) {
+        ProcessHandle currentProcess = ProcessHandle.current();
+        ProcessHandle.Info info = currentProcess.info();
+        System.out.println("Process Id: " + currentProcess.pid());
+        System.out.println("Direct children: " + currentProcess.children());
+        System.out.println("Class name: " + currentProcess.getClass());
+        System.out.println("All processes: " + ProcessHandle.allProcesses());
+        System.out.println("Process info: " + info);
+        System.out.println("Process command: " + info.command());
+        System.out.println("Process arguments: " + info.arguments());
+        System.out.println("Is process alive: " + currentProcess.isAlive());
+        System.out.println("Process's parent " + currentProcess.parent());
+
+
+        System.out.println("=============================================================");
+        System.out.println("Start notepad using process api");
+        ProcessBuilder pb = new ProcessBuilder("notepad.exe");
+        String np = "Not Present";
+        Process notepadProcess = null;
+        try {
+            notepadProcess = pb.start();
+            ProcessHandle.Info notepadInfo = notepadProcess.info();
+            System.out.printf("Process ID : %s%n", notepadProcess.pid());
+            System.out.printf("Command name : %s%n", notepadInfo.command().orElse(np));
+            System.out.printf("Command line : %s%n", notepadInfo.commandLine().orElse(np));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
