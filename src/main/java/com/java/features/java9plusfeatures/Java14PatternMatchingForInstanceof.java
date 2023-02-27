17e74ba7cc73e70e202044da1a0916c29001fb55
Filip Kulevski <filipkule@hotmail.com>
2023-02-27 20:21:06 +0100

    Minor refactor

--- src/main/java/com/java/features/java9plusfeatures/Java14PatternMatchingForInstanceof.java
+++ src/main/java/com/java/features/java9plusfeatures/Java14PatternMatchingForInstanceof.java
@@ -9,8 +9,8 @@ public class Java14PatternMatchingForInstanceof {
 
     public static double calculatePerimeter(Triangle triangle) {
         return switch (triangle) {
-            case EquivalentTriangle et -> 3 * et.getA();
-            case IsoscelesTriangle it -> 2 * it.getA() + it.getB();
+            case EquivalentTriangle et -> 3 * et.a();
+            case IsoscelesTriangle it -> 2 * it.a() + it.b();
             case ScalaneTriangle st -> st.getA() + st.getB() + st.getC();
             default -> throw new IllegalStateException("Unexpected value: " + triangle);
         };
@@ -19,36 +19,11 @@ public class Java14PatternMatchingForInstanceof {
     interface Triangle {
     }
 
-    static class EquivalentTriangle implements Triangle {
-        double a;
-        EquivalentTriangle (double a) {
-            this.a = a;
-        }
+    record EquivalentTriangle(double a) implements Triangle {}
 
-        public double getA() {
-            return a;
-        }
-    }
+    record IsoscelesTriangle(double a, double b) implements Triangle {}
 
-    static class IsoscelesTriangle implements Triangle {
-        double a;
-        double b;
-
-        IsoscelesTriangle(double a, double b) {
-            this.a = a;
-            this.b = b;
-        }
-
-        public double getA() {
-            return a;
-        }
-
-        public double getB() {
-            return b;
-        }
-    }
-
-    static class ScalaneTriangle implements Triangle {
+    static class ScalaneTriangle implements Triangle {// this one is not refactored into a record for the sake of comparison
         double a;
         double b;
         double c;
